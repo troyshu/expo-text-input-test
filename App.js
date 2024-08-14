c7fc7d9ed8f46135166d68e09429693a561d07e8
Troy Shu <tmshu1@gmail.com>
2024-08-14 11:59:41 -0400

    add email text input

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Email address</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        textContentType="emailAddress"
+        autoCapitalize="none"
+        autoComplete="email"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 5,
+    width: 200,
+    height: 40,
+    margin: 12,
+    padding: 10,
   },
 });
